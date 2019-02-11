e1d73e8b2076bdd2b79f4d54f7d31f0913c87496
AYM1607 <aym@Marianos-iMac.local>
2019-02-11 00:49:40 -0600

    Finished the ItemModel class

---
+++ lib/src/models/item_model.dart
@@ -0,0 +1,71 @@
+class ItemModel {
+  final String by;
+  final int descendants;
+  final int id;
+  final List<dynamic> kids;
+  final int score;
+  final int time;
+  final String title;
+  final String type;
+  final String url;
+  final bool deleted;
+  final String text;
+  final bool dead;
+  final int parent;
+  final int poll;
+  final List<dynamic> parts;
+
+  ItemModel(
+      {this.by,
+      this.descendants,
+      this.id,
+      this.kids,
+      this.score,
+      this.time,
+      this.title,
+      this.type,
+      this.url,
+      this.deleted,
+      this.text,
+      this.dead,
+      this.parent,
+      this.poll,
+      this.parts});
+
+  ItemModel.fromJson(Map<String, dynamic> json)
+  : by = json['by'],
+    descendants = json['descendants'],
+    id = json['id'],
+    kids = json['kids'],
+    score = json['score'],
+    time = json['time'],
+    title = json['title'],
+    type = json['type'],
+    url = json['url'],
+    deleted = json['deleted'],
+    text = json['text'],
+    dead = json['dead'],
+    parent = json['parent'],
+     poll = json['poll'],
+    parts = json['parts'];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['by'] = this.by;
+    data['descendants'] = this.descendants;
+    data['id'] = this.id;
+    data['kids'] = this.kids;
+    data['score'] = this.score;
+    data['time'] = this.time;
+    data['title'] = this.title;
+    data['type'] = this.type;
+    data['url'] = this.url;
+    data['deleted'] = this.deleted;
+    data['text'] = this.text;
+    data['dead'] = this.dead;
+    data['parent'] = this.parent;
+    data['poll'] = this.poll;
+    data['parts'] = this.parts;
+    return data;
+  }
+}
