52b239f25f67ffd4813b0294fa229a96a875d156
AYM1607 <aym@Marianos-iMac.local>
2019-02-11 00:16:39 -0600

    Added item model file

---
+++ lib/src/App.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hacker News client',
+      home: Text('new app'),
+    );
+  }
+}
