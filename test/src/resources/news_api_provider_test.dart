9ae1ff5a34afae6b9d753f100d478ad315a7dba8
AYM1607 <aym@Marianos-iMac.local>
2019-02-13 15:22:35 -0600

    Added tests for the api provider

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -5,4 +5,35 @@ import 'package:http/testing.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hacker_news_flutter_client/src/resources/news_api_provider.dart';
 
-void main() {}
+void main() {
+  test('fetchTopIds returns a list of ids', () async {
+    // Setup
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((Request request) async {
+      return Response(json.encode([1, 2, 3]), 200);
+    });
+    final topIds = await newsApi.fetchTopIds();
+
+    // Expectation
+    expect(topIds, [1, 2, 3]);
+  });
+
+  test('fetchItem returns an ItemModel', () async {
+    // Setup
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((Request request) async {
+      final jsonMap = {
+        'id': 1251,
+        'dead': false,
+        'text': "test",
+      };
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItem(123);
+    // Expectation
+    expect(item.id, 1251);
+    expect(item.dead, false);
+    expect(item.text, "test");
+  });
+}
