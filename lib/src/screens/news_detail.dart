11e15c3ef874408974af1d1aeb43c67a9291bc8d
AYM1607 <u.g.a.mariano@gmail.com>
2019-02-18 03:01:48 -0600

    Modified the news detail screen to work with the comments bloc and render title

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,16 +1,75 @@
 import 'package:flutter/material.dart';
 
+import '../blocs/comments_provider.dart';
+import '../models/item_model.dart';
+
 class NewsDetail extends StatelessWidget {
   final int itemId;
 
   NewsDetail({this.itemId});
 
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('News detail'),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWitComments,
+      builder: (BuildContext context,
+          AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          // At this point, no item has started the fetching process.
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        // At this point, the future corresponding to the top level item
+        // is already in the cache map.
+        final itemFuture = snapshot.data[itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder:
+              (BuildContext context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              // The fetching process for the top level item hasnt finished.
+              return Text('loading');
+            }
+            return buildList(itemSnapshot.data, snapshot.data);
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildList(ItemModel item, Map<int, Future<ItemModel>> itemMap) {
+    return ListView(
+      children: <Widget>[
+        buildTitle(item.title),
+      ],
+    );
+  }
+
+  Widget buildTitle(String title) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      alignment: Alignment.topCenter,
+      child: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24.0,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
